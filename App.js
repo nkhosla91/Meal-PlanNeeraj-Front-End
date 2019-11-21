d37f89b87eddba419796593b2d9ab0f6667d57ad
github username <github email address>
2019-11-21 10:46:10 -0500

    test

--- App.js
+++ App.js
@@ -1,31 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Button, Image} from 'react-native';
-// import { LinearGradient } from 'expo'
-import { DrawerNavigator } from 'react-navigation'
-import { Container, Content, Header, Icon, Body } from 'native-base';
+// import { DrawerNavigator } from 'react-navigation'
 import AppNavigation from './AppNavigation'
 import AuthNavigation from './AuthNavigation'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 
+const initialState = {
+  loggedIn: false
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type)
+  {
+    case 'LOG_IN':
+      return {counter: true}
+  }
+  return state 
+}
+
+const store = createStore(reducer)
 
 export default class App extends React.Component {
   
-  state = {
-    welcome: null
-  }
-
-  handleWelcome = (nav) => {v 
-    this.setState({welcome: nav})
-  }
-
   render (){
-    console.log(this.state)
-    return (
-      <AuthNavigation />
-    )
+    if(!this.state.loggedIn){
+      return (
+        <Provider store={store}>
+          <AuthNavigation />
+        </Provider>
+      )
+    } else if (this.state.loggedIn){
+      return (
+        <Provider store={store}>
+        <AppNavigation />
+      </Provider>
+      )
+    }
   }
 }//end of App Class
 
-
-const styles = StyleSheet.create({
-});
--- screens/Login.js
+++ screens/Login.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import { StyleSheet, Text, View, Button } from 'react-native'
+import {connect} from 'react-redux'
+import { runInThisContext } from 'vm'
 
-
-export default function Login() {
+function Login() {
   return (
     <View style={styles.container}>
       <Text>Login</Text>
-  
+      <Button
+          title="Log In"
+          onPress={() => this.props.logIn}
+        />
     </View>
   )
 }
+
+export default connect(mapDispatchToProps)(Login)
+
+
+function mapDispatchToProps(dispatch){
+  return {
+      logIn: () => dispatch({type: 'LOG_IN'}),
+  }
+}
+
 
 const styles = StyleSheet.create({
   container: {
