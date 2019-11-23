016ad191755280b90f6d28ef4ec1869cd64ed050
github username <github email address>
2019-11-23 11:00:22 -0500

    apptime

--- auth_screens/Signup.js
+++ auth_screens/Signup.js
@@ -41,14 +41,14 @@ class Signup extends React.Component {
   }
 
   handleState = (infoHash) => {
-    // console.log(infoHash)
+    // console.log(this.state)
     this.setState({...infoHash})
     // console.log(this.state.email, this.state.age)
     if (!this.state.email){
      this.props.navigation.navigate('PersonalInfo')
     
     } else if (!this.state.age){
-      console.log(infoHash["gender"], this.state)
+      // console.log(this.state)
               if (infoHash["gender"] === "M"){
                     const BMR = 66 + (6.23 * infoHash["weight"]) + (12.7 * infoHash["height"]) - (6.76 * infoHash["age"])
                     const calories = BMR * infoHash["activityLevel"]
@@ -59,25 +59,39 @@ class Signup extends React.Component {
                     const BMR = 655.1 + (4.35 * infoHash["weight"]) + (4.7 * infoHash["height"]) - (4.7 * infoHash["age"])
                     const calories = BMR * infoHash["activityLevel"]
                     this.setState({BMR, calories})
-                   console.log(this.state)
+                  //  console.log(this.state)
                    this.props.navigation.navigate('Macros')
               }//end of calorie if statement
-    } else {
-      //fetch post goes here************************************************************
+          
+    } else if (!this.state.proteinCalories){
       this.props.navigation.navigate('Summary')
-    }
+    } 
+  }
+
+  handleCreateUser = () => {
+    // console.log(this.state, "prefetch")
+      return fetch('http://192.168.0.29:3000/api/v1/users', {
+       method: 'POST',
+       headers: {
+         'Accept': 'application/json',
+         'Content-Type': 'application/json',
+       },
+       body: JSON.stringify(this.state),
+     }) 
+     .then(this.props.navigation.navigate('WelcomeScreen'))
   }
   
 
   render () {
-    console.log(this.state)
+    // console.log(this.state)
     const { navigation } = this.props
     const props = {
       handleState: this.handleState,
       calories: this.state.calories,
       carbCalories: this.state.carbCalories,
       proteinCalories: this.state.proteinCalories,
-      fatCalories: this.state.fatCalories
+      fatCalories: this.state.fatCalories,
+      handleCreateUser: this.handleCreateUser
     }
 
       return (
--- auth_screens/Summary.js
+++ auth_screens/Summary.js
@@ -6,10 +6,10 @@ import { LineChart, BarChart, Grid } from "react-native-chart-kit"
 
 export default class AccountInfo extends React.Component {
 
-    handleSubmit = () => {
-        const value = this._form.getValue()
-        this.props.screenProps.handleState(value)
-    }
+    // handleSubmit = () => {
+  
+    //     this.props.screenProps.handleCreateUser()
+    // }
 
     
     render () {
@@ -22,39 +22,44 @@ export default class AccountInfo extends React.Component {
                 </Text>
                 <View>
   
-  <BarChart
-    data={{
-      labels: ["Carbs", "Fat", "Protein"],
-      datasets: [
-        {
+                    <BarChart
+                      data={{
+                        labels: ["Carbs", "Fat", "Protein"],
+                        datasets: [
+                          {
 
-             data: [
-           this.props.screenProps.carbCalories,
-           this.props.screenProps.fatCalories,
-           this.props.screenProps.proteinCalories
-          ]
-        }
-      ]
-    }}
-    width={330} // from react-native
-    height={220}
-    chartConfig={{
-      backgroundColor: "#e26a00",
-      backgroundGradientFrom: "#fb8c00",
-      backgroundGradientTo: "#ffa726",
-      decimalPlaces: 0, // optional, defaults to 2dp
-      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-      style: {
-        borderRadius: 16
-      },
-    }}
-    style={{
-      marginVertical: 8,
-      borderRadius: 16
-    }}
-  />
-</View>
+                              data: [
+                            this.props.screenProps.carbCalories,
+                            this.props.screenProps.fatCalories,
+                            this.props.screenProps.proteinCalories
+                            ]
+                          }
+                        ]
+                      }}
+                      width={330} // from react-native
+                      height={220}
+                      chartConfig={{
+                        backgroundColor: "#e26a00",
+                        backgroundGradientFrom: "#fb8c00",
+                        backgroundGradientTo: "#ffa726",
+                        decimalPlaces: 0, // optional, defaults to 2dp
+                        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                        style: {
+                          borderRadius: 16
+                        },
+                      }}
+                      style={{
+                        marginVertical: 8,
+                        borderRadius: 16
+                      }}
+                    />
+                    
+                  </View>
+                <Button
+                  title="Create User"
+                  onPress={this.props.screenProps.handleCreateUser}
+                />
             </View>
         </KeyboardAwareScrollView>
     )
